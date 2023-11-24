b643a4504ff3c5484cc57d2602e8b2d6e7c86cdd
Felipe Cruz <104950263+FelipeCruzDev@users.noreply.github.com>
2023-11-24 08:48:31 -0300

    Update exercicio09.java
    Update

--- exercicio09.java
+++ exercicio09.java
@@ -12,7 +12,7 @@ public class exercicio09 {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um numero:");
         double resp = input.nextDouble();
-        String resultado = resp > 0 ? "O NUMERO É POSITIVO" : "O NUMERO É NEGATIVO";
+        String resultado =resp == 0 ? "NUMERO NEUTRO" : resp >=1 ?"O NUMERO É POSITIVO" : "O NUMERO É NEGATIVO";
         System.out.println(resultado);
         }
     }
