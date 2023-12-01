0bb26c3f47bcea18db34d236e0cdeba836ab6a64
Felipe Cruz <104950263+FelipeCruzDev@users.noreply.github.com>
2023-12-01 11:36:15 -0300

    Update exercicioArray03.java

--- ExercicioArrays/exercicioArray03.java
+++ ExercicioArrays/exercicioArray03.java
@@ -1,9 +1,19 @@
-package academy.aulas.maratonajava.introducao;
+package repeticoes;
+
+
 /*
 Escreva um código Java
 que exiba os valores de um array
-com o nome "a" do tipo double numa
+com o nome a do tipo double numa
 mesma linha.
  */
-public class exercicioArray03 {
+public class array {
+    public static void main(String[] args) {
+       double a [] = {1.5 ,1.6,1.9,8.0};
+       
+       for (int i = 0; i < a.length;i++ ) {
+    	   System.out.print(a[i] + " ");
+
+    }
+}
 }
