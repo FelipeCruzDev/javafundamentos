4bfe928624eab589d2dc761b0fa003482c31630d
Felipe Cruz <104950263+FelipeCruzDev@users.noreply.github.com>
2023-12-01 11:17:12 -0300

    Update exercicioArray01.java

--- ExercicioArrays/exercicioArray01.java
+++ ExercicioArrays/exercicioArray01.java
@@ -1,6 +1,6 @@
-package academy.aulas.maratonajava.introducao;
-
-public class exercicioArray01 {
+package repeticoes;
+import java.util.*;
+public class array {
     /*
 Para cada conjunto de
 valores abaixo, escreva o código
@@ -12,27 +12,51 @@ c) 1 2 3 4 5 10 20 30 40 50
 d) 3 4 7 12 19 28 39 52 67 84
      */
     public static void main(String[] args) {
-        int[] num1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1,};
-        int[] num2 = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
-        int[] num3 = {1, 2, 3, 4, 5, 10, 20, 30, 40, 50};
-        int[] num4 = {3, 4, 7, 12, 19, 28, 39, 52, 67, 84};
+    	Scanner input = new Scanner(System.in);
+    	
+    	int [] num1 = new int[5];
+    	int [] num2 = new int[5];
+    	int [] num3 = new int[5];
+    	int [] num4 = new int[5];
+    	
+    	
+    	for (int i = 0; i < num1.length;i++ ) {
+    	
+    	System.out.print("DIGITE OS VALORES: ");
+    	num1[i] = input.nextInt();
+    	}
 
-        for (int i : num1) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i : num2) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i : num3) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i : num4) {
-            System.out.print(i + " ");
+    	for (int x = 0;x < num2.length; x++) {
+    		System.out.print("DIGITE OS VALORES: ");
+        	num2[x] = input.nextInt();
+  
+    	}
+    	for (int y = 0;y < num3.length; y++) {
+    		System.out.print("DIGITE OS VALORES: ");
+        	num3[y] = input.nextInt();
+        
+        
+    	}
+    	for (int z = 0;z < num4.length; z++) {
+    		System.out.print("DIGITE OS VALORES: ");
+        	num4[z] = input.nextInt();
+        	 
+    }	
+    	 for (int i : num1) {
+             System.out.print(i + " ");
+         }
+         System.out.println();
+         for (int i : num2) {
+             System.out.print(i + " ");
+         }
+         System.out.println();
+         for (int i : num3) {
+             System.out.print(i + " ");
+         }
+         System.out.println();
+         for (int i : num4) {
+             System.out.print(i + " ");
 
-
-        }
     }
+}
 }
